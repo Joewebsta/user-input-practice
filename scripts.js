29b713637dbb76edd64edf230bd22ffc3dd62e4d
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-14 14:09:45 -0400

    Update box styles on click

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 let h1Text = document.querySelector('h1');
+let box = document.querySelector('.box');
 
 let textInput = document.querySelector('.text');
 let propertyInput = document.querySelector('#property');
@@ -7,9 +8,13 @@ let valueInput = document.querySelector('#value');
 let changeTextBtn = document.querySelector('.change-text');
 let changeStyleBtn = document.querySelector('.change-style');
 
-
-changeTextBtn.addEventListener('click', changeText);
-
 function changeText() {
   h1Text.innerText = textInput.value;
 }
+
+function changeStyle() {
+  box.style.cssText += `${propertyInput.value}: ${valueInput.value}`;  
+}
+
+changeTextBtn.addEventListener('click', changeText);
+changeStyleBtn.addEventListener('click', changeStyle);
